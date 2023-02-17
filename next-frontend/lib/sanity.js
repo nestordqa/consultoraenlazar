379e5e166379eade38c83c2af04dd4f94210db2c
Micaela Ceballos <micaelaceballos411@gmail.com>
2023-02-17 18:01:43 -0300

    fix sanity config, add responsive cards to Coursecard component

--- next-frontend/lib/sanity.js
+++ next-frontend/lib/sanity.js
@@ -1,8 +1,16 @@
 import { definePreview } from "next-sanity/preview";
-import { createCurrentUserHook } from "next-sanity";
 import createImageUrlBuilder from "@sanity/image-url";
 import { config } from "./config";
+import { suspend } from "suspend-react";
+import { _checkAuth } from "@sanity/preview-kit";
 
 export const urlFor = (source) => createImageUrlBuilder(config).image(source);
 export const usePreviewSubscription = definePreview(config);
-//export const useCurrentUser = createCurrentUserHook(config);
+
+const projectId = config.projectId;
+const token = config.token;
+export const useCheckAuth = () =>
+  suspend(
+    () => _checkAuth(projectId, token),
+    ["@sanity/preview-kit", "checkAuth", projectId, token]
+  );
