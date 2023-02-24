9a332699f8347868641e3245e1d57183e25843c7
Jonatan P <jonatan2502@gmail.com>
2023-02-24 18:51:18 -0400

    Add headline for team section

--- next-frontend/pages/team/index.jsx
+++ next-frontend/pages/team/index.jsx
@@ -9,10 +9,16 @@ const Team = ({ team }) => {
         title={"Equipo"}
         content={"Equipo de trabajo de Consultora Enlazar."}
       >
-        <h1>
-          Hola mundo desde Equipo!
-        </h1>
-        <TeamCards team={team}/>
+      <div className="flex flex-col content-center">
+        <div className="flex justify-center items-center w-full h-auto pb-6 md:pb-12 bg-grey">
+            <h3 className="pt-16 px-4 md:px-8 text-2xl md:text-3xl font-bold text-center text-ellipsis opacity-90">
+              Conoce a nuestro gran equipo...
+            </h3>
+        </div>
+        <div class='flex justify-center py-12'>
+          <TeamCards team={team}/>
+        </div>
+      </div>
       </Layout>
     </>
   );
