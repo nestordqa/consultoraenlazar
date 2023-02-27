412c04eca39cf8f018629ab9313057943ccdc16d
Jonatan P <jonatan2502@gmail.com>
2023-02-27 15:28:39 -0400

    change div to section

--- next-frontend/pages/team/index.jsx
+++ next-frontend/pages/team/index.jsx
@@ -1,5 +1,6 @@
 import TeamCards from "@/components/card/TeamCards";
 import { Layout } from "@/components/Layout";
+import Values from "@/components/values/values";
 import { loadTeam } from "@/lib/loadTeamMembers";
 
 const Team = ({ team }) => {
@@ -10,14 +11,17 @@ const Team = ({ team }) => {
         content={"Equipo de trabajo de Consultora Enlazar."}
       >
       <div className="flex flex-col content-center">
-        <div className="flex justify-center w-full h-40 bg-grey">
+        <section className="flex justify-center w-full h-40 bg-grey">
             <h3 className="text-2xl md:text-3xl font-bold text-center text-ellipsis opacity-90">
               Conoce a nuestro gran equipo...
             </h3>
-        </div>
-        <div className='flex justify-around py-12'>
+        </section>
+        <section className='flex justify-around py-12'>
           <TeamCards team={team}/>
-        </div>
+        </section>
+        <section>
+          <Values/>
+        </section>
       </div>
       </Layout>
     </>
