a45f8fe0bb2067ab67e38e330a1104773ba12465
Jonatan P <jonatan2502@gmail.com>
2023-02-28 15:45:00 -0400

    change bg-color

--- next-frontend/components/workWithUs/WorkWithUs.jsx
+++ next-frontend/components/workWithUs/WorkWithUs.jsx
@@ -3,13 +3,13 @@ import WorkWithUsImage from 'public/images/trabaja-con-nosotros.webp'
 
 const WorkWithUs = () => {
     return (
-        <div className="flex justify-center bg-dark">
+        <div className="flex justify-center bg-grey">
             <div className='xsm:flex w-9/12 xsm:justify-center xsm:flex-wrap lg:flex-nowrap py-16 '>
                 <div className="pr-24">
-                    <h2 className="text-2xl md:text-3xl font-bold text-ellipsis opacity-90 pb-12 text-yellow">
+                    <h2 className="text-2xl md:text-3xl font-bold text-ellipsis opacity-90 pb-12 text-darkBlue">
                     ¡Trabajá con nosotros!
                     </h2>
-                    <p className="text-white pb-8">
+                    <p className="text-dark pb-8">
                         Constantemente estamos ampliando nuestra red de profesionales. Si te sentís identificado/a con nuestra organización, podés dejarnos tu CV y lo tendremos en cuenta para futuras búsquedas.
                     </p>
                     <button
