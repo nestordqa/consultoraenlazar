7140bc2245641e38871eddfb58729aefaeeacd26
Jonatan P <jonatan2502@gmail.com>
2023-02-28 00:23:26 -0400

    remove titles

--- next-frontend/pages/team/index.jsx
+++ next-frontend/pages/team/index.jsx
@@ -1,6 +1,7 @@
 import TeamCards from "@/components/card/TeamCards";
 import { Layout } from "@/components/Layout";
 import Values from "@/components/values/Values";
+import WorkWithUs from "@/components/workWithUs/WorkWithUs";
 import { loadTeam } from "@/lib/loadTeamMembers";
 
 const Team = ({ team }) => {
@@ -11,16 +12,14 @@ const Team = ({ team }) => {
         content={"Equipo de trabajo de Consultora Enlazar."}
       >
       <div className="flex flex-col content-center">
-        <section className="flex justify-center w-full h-40 bg-grey">
-            <h3 className="text-2xl md:text-3xl font-bold text-center text-ellipsis opacity-90">
-              Conoce a nuestro gran equipo...
-            </h3>
-        </section>
-        <section className='flex justify-around py-12'>
+        <section>
           <TeamCards team={team}/>
         </section>
         <section>
           <Values/>
+        </section>
+        <section >
+          <WorkWithUs/>
         </section>
       </div>
       </Layout>
