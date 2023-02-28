5c5c3819dca6118c4928269d74200b08699d5b39
Jonatan P <jonatan2502@gmail.com>
2023-02-28 15:44:46 -0400

    update WeAre component

--- next-frontend/components/home/WeAre.jsx
+++ next-frontend/components/home/WeAre.jsx
@@ -1,9 +1,22 @@
 import React from 'react';
+import TeamCards from '../card/TeamCards';
+import Values from '../values/values';
+import WorkWithUs from '../workWithUs/WorkWithUs';
 
-export const WeAre = () => {
+export const WeAre = ({team}) => {
   return (
-    <div className='bg-dark w-full h-[calc(100vh-112px)]'>
-        WeAre
-    </div>
+    <div className="flex flex-col content-center">
+        <section>
+          <TeamCards team={team}/>
+        </section>
+        <section>
+          <Values/>
+        </section>
+        <section >
+          <WorkWithUs/>
+        </section>
+      </div>
   );
 };
+
+export default WeAre
