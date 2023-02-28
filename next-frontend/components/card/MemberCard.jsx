c8a3f63068a5fe022c88ec4d04fd98d360c3b5d1
Jonatan P <jonatan2502@gmail.com>
2023-02-28 00:10:22 -0400

    change css

--- next-frontend/components/card/MemberCard.jsx
+++ next-frontend/components/card/MemberCard.jsx
@@ -2,15 +2,11 @@ import { urlFor } from "@/lib/sanity";
 
 const MemberCard = ({ name, image, jobTitle}) => {
     return (
-        <div className='box-border py-4 px-2 w-56 '>
+        <div className='box-border py-4 px-2 w-56 text-white'>
             <img 
                 src={urlFor(image).url()}
                 className="w-56"
-                // width={200}
-                // height={200}
-
-                >
-            </img>
+            />
             <strong>{name}</strong>
             <p>{jobTitle}</p>
         </div>
