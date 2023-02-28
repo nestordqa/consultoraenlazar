6e5adeb7a3605c105437df77cf0df84b25cb10c1
Jonatan P <jonatan2502@gmail.com>
2023-02-28 15:57:48 -0400

    Change 'img' tag to 'image'

--- next-frontend/components/card/MemberCard.jsx
+++ next-frontend/components/card/MemberCard.jsx
@@ -1,13 +1,16 @@
 import { urlFor } from "@/lib/sanity";
+import Image from "next/image";
 
 const MemberCard = ({ name, image, jobTitle}) => {
     return (
         <div className='box-border py-4 px-2 w-56 text-white'>
-            <img 
+            <Image 
                 src={urlFor(image).url()}
                 className="w-56"
                 alt='Foto de perfil'
-            />
+                width="800"
+                height="800"
+            ></Image>
             <strong>{name}</strong>
             <p>{jobTitle}</p>
         </div>
