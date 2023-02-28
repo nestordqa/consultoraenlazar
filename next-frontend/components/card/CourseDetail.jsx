cc7949d75265f6c31c5159b5c0cc0e1b7985fb5f
Micaela Ceballos <83962558+MicaCblls@users.noreply.github.com>
2023-02-28 10:41:39 -0300

    Update CourseDetail.jsx

--- next-frontend/components/card/CourseDetail.jsx
+++ next-frontend/components/card/CourseDetail.jsx
@@ -38,13 +38,36 @@ const DetailsComponents = {
       return <p className="px-2 pt-2 pb-4">{children}</p>;
     },
   },
+    marks: {
+    em: ({ children }) => (
+      <em className="text-dark font-semibold">{children}</em>
+    ),
+    strong: ({ children }) => (
+      <strong className="text-blue font-semibold">{children}</strong>
+    ),
+    link: ({ children, value }) => {
+      const target = (value?.href || "").startsWith("http")
+        ? "_blank"
+        : undefined;
+      return (
+        <a
+          href={value?.href}
+          target={target}
+          rel={target === "_blank" && "noindex nofollow"}
+          className="underline text-blue font-semibold"
+        >
+          {children}
+        </a>
+      );
+    },
+  },
 };
 
 export default function Details({ handleOpenForm, handleClose, open, course }) {
   return (
     <>
       <div className="fixed z-[300] inset-0 font-Noah overflow-y-auto">
-        <div className="flex items-center justify-center max-h-screen pt-32 px-4 pb-5 md:pb-20 text-center sm:block sm:p-0">
+        <div className="flex items-center justify-center max-h-screen pt-10 px-4 pb-5 text-center sm:block sm:p-0">
           <div className="fixed inset-0 transition-opacity" aria-hidden="true">
             <div className="absolute inset-0 bg-black opacity-50"></div>
           </div>
@@ -76,11 +99,39 @@ export default function Details({ handleOpenForm, handleClose, open, course }) {
               </h3>
             </div>
 
-            <div className={styles.contentScroll}>
+             <div className={styles.contentScroll}>
               <PortableText
                 value={course.body}
                 components={DetailsComponents}
               />
+              {course.commissions !== null && course.commissions.length >= 1 ? (
+                <>
+                  <h3 className="py-4 px-2 text-2xl text-blue border-y-[1px]">
+                    <strong>Próximas comisiones:</strong>
+                  </h3>
+                  {course.commissions.map((item) => (
+                    <ul key={item._id} className="py-4 px-6">
+                      <li className="list-disc py-2">
+                        <strong className="text-blue font-semibold">
+                          {item.duration}
+                        </strong>
+                      </li>
+                      <li className="pl-4 py-1">
+                        <strong className="text-blue font-semibold">
+                          Duración:{" "}
+                        </strong>
+                        {item.dates}
+                      </li>
+                      <li className="pl-4 py-1">
+                        <strong className="text-blue font-semibold">
+                          Modalidad de cursada:{" "}
+                        </strong>
+                        {item.modality}
+                      </li>
+                    </ul>
+                  ))}
+                </>
+              ) : null}
             </div>
 
             <div className="bg-white border-t rounded-b-lg px-4 py-1 sm:px-6 sm:flex sm:flex-row-reverse">
