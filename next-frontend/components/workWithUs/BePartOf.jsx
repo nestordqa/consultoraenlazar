e46bf08ac43e4ccb54d9cf05de518e56b960961e
Jonatan P <jonatan2502@gmail.com>
2023-03-03 18:46:49 -0400

    fix bg img

--- next-frontend/components/workWithUs/BePartOf.jsx
+++ next-frontend/components/workWithUs/BePartOf.jsx
@@ -1,10 +1,21 @@
-const BePartOf = () => {
-    return (
-        <div className=" bg-[url('../public/images/letra-e-completa.png')] h-[60vh] flex flex-col justify-center items-center bg-repeat-x bg-auto bg-center">
-          <h2 className="text-[#34668a] text-2xl">SÉ PARTE DEL  <strong>FUTURO</strong> DEL<br />
-            <strong className="pl-9">CAPITAL HUMANO</strong></h2>
-        </div>
-    )
-}
+import Image from "next/image";
+import BackgrorundImg from "@/public/images/letra-e-completa.png";
 
-export default BePartOf
+const BePartOf = () => {
+	return (
+		<div className='flex justify-center items-center h-[34rem]'>
+			<div className='absolute items-center'>
+				<Image src={BackgrorundImg} width={600} height={600} alt='Background Img'/>
+			</div>
+			<div className='absolute items-center'>
+				<h2 className='text-blue text-2xl text-center'>
+					SÉ PARTE DEL <strong>FUTURO</strong> DEL
+					<br />
+					<strong className=''>CAPITAL HUMANO</strong>
+				</h2>
+			</div>
+		</div>
+	);
+};
+
+export default BePartOf;
