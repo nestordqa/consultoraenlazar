6a8df8682a93543a68844426e3c889733f773e06
Jonatan P <jonatan2502@gmail.com>
2023-03-10 13:39:30 -0400

    add ContactUsForm

--- next-frontend/pages/index.js
+++ next-frontend/pages/index.js
@@ -1,33 +1,35 @@
-import { Layout } from "@/components/Layout";
-import { ContactUs } from "@/components/home/ContactUs";
-import { Feedback } from "@/components/home/Feedback";
-import { Goal } from "@/components/home/Goal";
-import { Landing } from "@/components/home/Landing";
-import { LearnWithUs } from "@/components/home/LearnWithUs";
-import { OurServices } from "@/components/home/OurServices";
-import { WeAre } from "@/components/home/WeAre";
-import { CompaniesThatTrust } from "@/components/home/CompaniesThatTrust";
+import { Layout } from '@/components/Layout';
+import { ContactUs } from '@/components/home/ContactUs';
+import { Feedback } from '@/components/home/Feedback';
+import { Goal } from '@/components/home/Goal';
+import { Landing } from '@/components/home/Landing';
+import { LearnWithUs } from '@/components/home/LearnWithUs';
+import { OurServices } from '@/components/home/OurServices';
+import { WeAre } from '@/components/home/WeAre';
+import { CompaniesThatTrust } from '@/components/home/CompaniesThatTrust';
+import ContactUsForm from '@/components/forms/ContactUsForm';
 
 const Home = () => {
-  return (
-    <>
-      <Layout
-        title={"Inicio"}
-        content={"Consultora Enlazar - Se parte del futuro del capital Humano"}
-      >
-        <>
-          <Landing />
-          <CompaniesThatTrust />
-          {/*<WeAre/>
+	return (
+		<>
+			<Layout
+				title={'Inicio'}
+				content={'Consultora Enlazar - Se parte del futuro del capital Humano'}
+			>
+				<>
+					<Landing />
+					<CompaniesThatTrust />
+					<ContactUsForm />
+					{/*<WeAre/>
           <Goal/>
           <OurServices/>
           <LearnWithUs/>
           <Feedback/>
           <CompaniesThatTrust/>
           <ContactUs/>*/}
-        </>
-      </Layout>
-    </>
-  );
+				</>
+			</Layout>
+		</>
+	);
 };
 export default Home;
