f48d789447161405c87d946886310994704c77d8
Jonatan P <jonatan2502@gmail.com>
2023-03-12 19:33:47 -0400

    css: responsive form size

--- next-frontend/components/forms/ContactUsForm.jsx
+++ next-frontend/components/forms/ContactUsForm.jsx
@@ -1,4 +1,6 @@
 import { useState, useEffect } from 'react';
+import Image from 'next/image';
+import BackgroundImg from '@/public/images/letra-e-completa.png';
 
 const ContactUsForm = () => {
 	const [input, setData] = useState({
@@ -71,118 +73,135 @@ const ContactUsForm = () => {
 
 	return (
 		<>
-			<div className='flex items-center justify-center max-h-screen pt-10 px-4 pb-5 text-center sm:block sm:p-0'>
-				<div className='inline-block align-bottom rounded-lg text-left shadow-2xl transform transition-all my-2 sm:align-middle sm:max-w-2xl sm:w-full bg-white'>
-					<div className='px-12 py-12 rounded-t-lg'>
-						<div className='pt-4'>
-							<h2 className='text-darkBlue px-2 text-2xl lg:text-3xl font-semibold text-ellipsis text-center'>
-              ¿Cómo podemos ayudarte?
-							</h2>
-							<p className='p-3 pb-12 font-semibold text-ellipsis text-center text-base'>
-								Envianos tu consulta y nos pondremos en contacto a la brevedad.
-							</p>
-						</div>
-						<form
-							onSubmit={handleSubmit}
-							action='https://formsubmit.co/hola@enlazar.xyz'
-							method='POST'
-							encType='multipart/form-data'
-						>
-							<div className='w-full flex flex-col md:flex-row md:mb-2 justify-evenly items-center'>
-								<div className='flex flex-col w-full mb-2 justify-center md:w-2/4 md:mr-2 md:mb-0'>
-									<input
-										type='text'
-										name='name'
-										value={input.name}
-										placeholder='Nombre y Apellido'
-										onChange={handleOnChange}
-										className='flex w-full h-[3.3rem] py-2 px-4 border border-solid border-grey rounded-xl text-lg'
-										required
-									/>
-									<small className='h-6 text-red-600'>{errors.name}</small>
-								</div>
-								<div className='flex flex-col w-full mt-2 justify-center md:w-2/4 md:ml-2 md:mt-0'>
-									<input
-										type='text'
-										name='company'
-										value={input.company}
-										placeholder='Empresa'
-										onChange={handleOnChange}
-										className='flex justify-between w-full h-[3.3rem] py-2 px-4 border border-solid border-grey rounded-xl text-lg '
-										required
-									/>
-									<small className='h-6 text-red-600'>{errors.company}</small>
-								</div>
-							</div>
-              <div className='w-full flex flex-col md:flex-row md:mb-2 justify-evenly items-center'>
-								<div className='flex flex-col w-full mb-2 justify-center md:w-2/4 md:mr-2 md:mb-0'>
-									<input
-										type='email'
-										name='email'
-										value={input.email}
-										placeholder='Email'
-										onChange={handleOnChange}
-										className='flex w-full py-2 px-4 border border-solid border-grey rounded-xl text-lg'
-										required
-									/>
-									<small className='h-6 text-red-600'>{errors.email}</small>
-								</div>
-                <div className='flex flex-col w-full mt-2 justify-center md:w-2/4 md:ml-2 md:mt-0'>
-									<input
-										type='tel'
-										name='phone'
-										value={input.phone}
-										placeholder='Teléfono'
-										onChange={handleOnChange}
-										className='flex justify-between w-full h-[3.3rem] py-2 px-4 border border-solid border-grey rounded-xl text-lg '
-										required
-									/>
-									<small className='h-6 text-red-600'>{errors.phone}</small>
-								</div>
-							</div>
-							<div className='flex flex-col w-full justify-start md:mb-2 md:h-2/4'>
-								<textarea
-									name='comments'
-									value={input.comments}
-									placeholder='Contanos un poco sobre vos'
+			<div className='border flex flex-col justify-center items-center md:h-[44rem] xsm:h-[34rem] md:w-screen text-center'>
+				<div className=''>
+					<Image
+						src={BackgroundImg}
+						width={600}
+						height={600}
+						alt='Background Img'
+						priority
+						className='w-auto h-auto'
+					/>
+				</div>
+				<div className='absolute rounded-lg w-fit content-center transition-all bg-transparent p-12'>
+					<div className='md:pt-4'>
+						<h2 className='text-darkBlue md:px-2 xsm:text-xl lg:text-3xl font-semibold text-ellipsis text-center'>
+							¿Cómo podemos ayudarte?
+						</h2>
+						<p className='xsm:p-1 xsm:pb-8 md:p-3 md:pb-12 font-semibold text-ellipsis text-center xsm:text-sm lg:text-base'>
+							Envianos tu consulta y nos pondremos en contacto a la brevedad.
+						</p>
+					</div>
+					<form
+						onSubmit={handleSubmit}
+						action='https://formsubmit.co/hola@enlazar.xyz'
+						method='POST'
+						encType='multipart/form-data'
+					>
+						<div className='w-full flex flex-col md:flex-row md:mb-2 justify-evenly items-center'>
+							<div className='flex flex-col w-full justify-center md:w-2/4 md:mr-2 md:mb-0'>
+								<input
+									type='text'
+									name='name'
+									value={input.name}
+									placeholder='Nombre y Apellido'
 									onChange={handleOnChange}
-									rows={8}
-									className='flex w-full max-h-24 py-2 px-4 border border-solid border-grey rounded-xl text-lg resize-none mb-3 md:h-3/4'
+									className='flex w-full xsm:h-8 md:h-[3.3rem] xsm:py-1 xsm:px-4 md:py-2 md:px-4 border border-solid border-grey rounded-xl xsm:text-xs md:text-lg bg-transparent'
 									required
 								/>
-								<div className='flex justify-between'>
-									<small className='h-6 text-red-600'>{errors.comments}</small>
-									<small
-										className={
-											errors.comments
-												? 'h-6 text-red-600'
-												: 'h-6 text-green-600'
-										}
-									>
-										{!input.comments.length ? '' : input.comments.length}
-									</small>
-								</div>
+								<small className='h-6 text-red-600 xsm:text-xs md:text-[13px] text-left'>
+									{errors.name}
+								</small>
 							</div>
-							<div className='flex pt-3 justify-end self-center'>
-								<div className='pl-5'>
-									<button
-										className={
-											isDisabled
-												? 'bg-grey text-gray-500 rounded-2xl py-1 px-8 font-semibold uppercase self-center md:self-end'
-												: 'bg-yellow rounded-2xl py-1 px-8 font-semibold uppercase self-center md:self-end'
-										}
-										type='submit'
-										disabled={isDisabled}
-									>
-										Enviar consulta
-									</button>
-								</div>
+							<div className='flex flex-col w-full justify-center md:w-2/4 md:ml-2 md:mt-0'>
+								<input
+									type='text'
+									name='company'
+									value={input.company}
+									placeholder='Empresa'
+									onChange={handleOnChange}
+									className='flex justify-between w-full xsm:h-8 md:h-[3.3rem] xsm:py-1 xsm:px-4 md:py-2 md:px-4 border border-solid border-grey rounded-xl xsm:text-xs md:text-lg bg-transparent'
+									required
+								/>
+								<small className='h-6 text-red-600 xsm:text-xs md:text-[13px] text-left'>
+									{errors.company}
+								</small>
 							</div>
-						</form>
-					</div>
+						</div>
+						<div className='w-full flex flex-col md:flex-row md:mb-2 justify-evenly items-center'>
+							<div className='flex flex-col w-full mb:mb-2 justify-center md:w-2/4 md:mr-2'>
+								<input
+									type='email'
+									name='email'
+									value={input.email}
+									placeholder='Email'
+									onChange={handleOnChange}
+									className='flex w-full xsm:h-8 md:h-[3.3rem] xsm:py-1 xsm:px-4 md:py-2 md:px-4 border border-solid border-grey rounded-xl xsm:text-xs md:text-lg bg-transparent'
+									required
+								/>
+								<small className='h-6 text-red-600 xsm:text-xs md:text-[13px] text-left'>
+									{errors.email}
+								</small>
+							</div>
+							<div className='flex flex-col w-full justify-center md:w-2/4 md:ml-2 md:mt-0'>
+								<input
+									type='tel'
+									name='phone'
+									value={input.phone}
+									placeholder='Teléfono'
+									onChange={handleOnChange}
+									className='flex justify-between w-full xsm:h-8 md:h-[3.3rem] xsm:py-1 xsm:px-4 md:py-2 md:px-4 border border-solid border-grey rounded-xl xsm:text-xs md:text-lg bg-transparent'
+									required
+								/>
+								<small className='h-6 text-red-600 xsm:text-xs md:text-[13px] text-left'>
+									{errors.phone}
+								</small>
+							</div>
+						</div>
+						<div className='flex flex-col w-full justify-start xsm:mb-0 md:mb-2 md:h-2/4'>
+							<textarea
+								name='comments'
+								value={input.comments}
+								placeholder='Contanos un poco sobre vos'
+								onChange={handleOnChange}
+								rows={8}
+								className='flex w-full xsm:h-16 xsm:py-3 xsm:px-4 md:py-2 md:px-4 border border-solid border-grey rounded-xl xsm:text-xs md:text-lg resize-none md:mb-3 md:h-3/4 bg-transparent'
+								required
+							/>
+							<div className='flex justify-between'>
+								<small className='h-6 text-red-600 xsm:text-xs md:text-[13px] text-left'>
+									{errors.comments}
+								</small>
+								<small
+									className={
+										errors.comments
+											? 'xsm:h-2 md:h-6 text-red-600 xsm:text-xs md:text-[13px]'
+											: 'xsm:h-2 md:h-6 text-green-600 xsm:text-xs md:text-[13px]'
+									}
+								>
+									{!input.comments.length ? '' : input.comments.length}
+								</small>
+							</div>
+						</div>
+						<div className='flex pt-3 justify-end self-center'>
+							<div className='pl-5'>
+								<button
+									className={
+										isDisabled
+											? 'bg-grey text-gray-500 rounded-2xl py-1 px-8 font-semibold uppercase self-center md:self-end'
+											: 'bg-yellow rounded-2xl py-1 px-8 font-semibold uppercase self-center md:self-end'
+									}
+									type='submit'
+									disabled={isDisabled}
+								>
+									Enviar consulta
+								</button>
+							</div>
+						</div>
+					</form>
 				</div>
 			</div>
-			{/* </div> */}
 		</>
 	);
 };
