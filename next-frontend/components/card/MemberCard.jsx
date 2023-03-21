7f44f25671a63417a6d159c607f837de5e549e45
Jonatan P <jonatan2502@gmail.com>
2023-03-21 14:53:34 -0400

    update teamCards

--- next-frontend/components/card/MemberCard.jsx
+++ next-frontend/components/card/MemberCard.jsx
@@ -1,7 +1,7 @@
 import { urlFor } from "@/lib/sanity";
 import Image from "next/image";
 
-const MemberCard = ({ name, image, jobTitle}) => {
+const MemberCard = ({ name, image, jobFirstLine, jobSecondLine}) => {
     return (
         <div className='box-border py-4 px-2 w-56 text-white'>
             <Image 
@@ -11,8 +11,9 @@ const MemberCard = ({ name, image, jobTitle}) => {
                 width="800"
                 height="800"
             ></Image>
-            <p className="text-center font-bold">{name}</p>
-            <p className="text-center">{jobTitle}</p>
+            <p className="text-center font-bold pt-4">{name}</p>
+            <p className="text-center">{jobFirstLine}</p>
+            <p className="text-center">{jobSecondLine}</p>
         </div>
     )
 }
