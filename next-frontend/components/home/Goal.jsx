f72ae7b6ce3441ee140ba666febd6eb977dd1bc6
Nancy Clavijo Varela <clavijovarela@gmail.com>
2023-03-21 10:18:45 -0500

    Detalles

--- next-frontend/components/home/Goal.jsx
+++ next-frontend/components/home/Goal.jsx
@@ -5,28 +5,26 @@ import styles from "./Companies.module.css";
 
 export const Goal = () => {
   return (
-    <div class="bg-darkBlue w-full ">
-    <div class="mr-auto max-w-7xl pr-4 sm:pr-2 lg:pr-2  ">
-        <div class=" space-y-12 lg:grid lg:grid-cols-2 lg:gap-x-6 "> 
-        <div class="group relative">
+    <div class="goal-container  py-12">
+    <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 flex justify-center flex-wrap ">
+        
+        <div class="goal-image w-full lg:w-1/2 mb-10 lg:mb-0">
             <div class= {styles.image}>
             <Image     
            className='w-full h-full '
            alt= " image"
            src="/images/nuestro-objetivo.webp"
-           width={300}
-           height={300}
+           width={200}
+           height={200}
             />
             </div>
           </div>
-          <div class="group relative pl-10">
-          <h2 className=" pb-10 px-4 sm:px-8 text-1xl lg:text-2xl font-bold text-center text-ellipsis opacity-90 text-white flex justify-end">
-        Somos Enlazar  
-          </h2>  
-       <p class=" text-right  text-ellipsis text-white sm:w-4/5 md:w-3/4 lg:w-full mx-auto flex justify-end">
+          <div class="goal-content w-full lg:w-1/2 pl-5 lg:pl-24 flex flex-col justify-center">
+          <h2 className="text-2xl lg:text-3xl font-bold text-center text-ellipsis text-gray-800 mb-8">Somos Enlazar</h2>
+       <p class="text-gray-800 text-base lg:text-lg mb-8">
             Nuestra misión: Revolucionar el área de RRHH. Creamos y brindamos soluciones de desarrollo para personas, equipos y organizaciones en materia de formación profesional y consultoría, basadas en la experiencia, la innovación y las buenas prácticas, apoyándonos en el valor humano y las nuevas tecnologías.
               </p>
-              <div className="flex justify-end mt-6    pt-10 ">
+              <div className="goal-button flex justify-center">
               <Link href="/team">
                         <button className="rounded-full  bg-yellow p-1 px-6 font-semibold">Conoce nuestro Equipo</button>
                </Link>        
@@ -36,7 +34,7 @@ export const Goal = () => {
         </div>
       </div>
    
-  </div>
+ 
          
    
   );
