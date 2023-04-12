ed8ed485aa57e40e00249a162e6edfd8ea03b4f4
Nancy Clavijo Varela <clavijovarela@gmail.com>
2023-04-12 08:30:46 -0500

    Mejora

--- next-frontend/components/home/Goal.jsx
+++ next-frontend/components/home/Goal.jsx
@@ -6,17 +6,18 @@ import Link from "next/link";
 export const Goal = () => {
   return (
 
-    <div class="goal-container flex flex-wrap ">
-         <div class="goal-image w-full lg:w-1/3 mb-10 lg:mb-0  "> 
+    <div class="goal-container flex flex-wrap w-full h-auto ">
+         <div class="goal-image w-full lg:w-1/2 mb-10 lg:mb-0  "> 
             <Image     
            class='w-full h-full '
            alt= " image"
            src="/images/nuestro-objetivo.webp"
            width={500}
            height={500}
+           quality={100}
             />
           </div>
-          <div class="goal-content w-full lg:w-1/2 pl-5 lg:pl-24 flex flex-col justify-center">
+          <div class="goal-content w-full lg:w-1/2 pl-5 flex flex-col justify-center">
           <h2 class="text-2xl lg:text-3xl font-bold text-center text-ellipsis text-gray-800 mb-8">Somos Enlazar</h2>
        <p class="text-gray-800 text-base lg:text-lg mb-8">
             Nuestra misión: Revolucionar el área de RRHH. Creamos y brindamos soluciones de desarrollo para personas, equipos y organizaciones en materia de formación profesional y consultoría, basadas en la experiencia, la innovación y las buenas prácticas, apoyándonos en el valor humano y las nuevas tecnologías.
--- next-frontend/pages/index.js
+++ next-frontend/pages/index.js
@@ -8,6 +8,8 @@ import { OurServices } from "@/components/home/OurServices";
 import { WeAre } from "@/components/home/WeAre";
 import { CompaniesThatTrust } from "@/components/home/CompaniesThatTrust";
 import ContactUsForm from "@/components/forms/ContactUsForm";
+import { getClient } from "@/lib/sanity.server";
+import groq from "groq";
 
 const Home = () => {
   return (
@@ -32,3 +34,20 @@ const Home = () => {
   );
 };
 export default Home;
+
+export async function getStaticProps() {
+  const companies = await getClient().fetch(
+    groq`*[_type == "company"] | order(title asc){
+      _id,
+      title,
+      mainImage,
+    }`
+  );
+
+  return {
+    props: {
+      companies,
+    },
+    revalidate: 1,
+  };
+}
